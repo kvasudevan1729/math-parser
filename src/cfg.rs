a69f1d841a6930b49e74818eb59f5cf005d12839
Krish Vasudevan <krish.vasudevan@cellularorigins.com>
2025-03-08 21:22:41 +0000

    Add error handling with custom errors

--- src/cfg.rs
+++ src/cfg.rs
@@ -77,21 +77,48 @@ impl fmt::Display for ParseNode {
     }
 }
 
+#[derive(Debug)]
+pub(crate) enum ParseError {
+    EndOfTokenError,
+    InvalidTokenError(String),
+    UnknownError,
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ParseError::EndOfTokenError => write!(f, "Reached end of token stream!"),
+            ParseError::InvalidTokenError(s) => write!(f, "Invalid token found: {}", s),
+            ParseError::UnknownError => write!(f, "Unknown error during parsing!"),
+        }
+    }
+}
+
+/// check if we have reached EOF by inspecting the current position with the
+/// number of tokens
+fn get_next_token(
+    tokens: &Vec<LexToken>,
+    pos: usize,
+) -> Result<(Option<&LexToken>, usize), ParseError> {
+    if (pos + 1) >= tokens.len() {
+        return Err(ParseError::EndOfTokenError);
+    }
+    let tok = tokens.get(pos + 1);
+    println!("=> tok: {:?}", tok);
+    return Ok((tok, pos + 1));
+}
+
 /// parsing top level expression
-fn parse_expr(tokens: &Vec<LexToken>, pos: usize) -> Result<(ParseNode, usize), String> {
+fn parse_expr(tokens: &Vec<LexToken>, pos: usize) -> Result<(ParseNode, usize), ParseError> {
     println!("=> parsing expr node at position {pos}");
     let (multi_expr_node, new_pos) = parse_multi_expr(&tokens, pos)?;
     let mut expr_node = ParseNode::new(CfgTerm::NonTermExpr);
     expr_node.add_child_node(multi_expr_node);
 
-    // check if we have reached EOF
-    if (new_pos + 1) >= tokens.len() {
-        return Ok((expr_node, new_pos));
-    }
-
     // look for +
     println!("=> looking for +/- ... {new_pos}");
-    let tok = tokens.get(new_pos + 1);
+    // let tok = tokens.get(new_pos + 1);
+    let (tok, next_pos) = get_next_token(&tokens, new_pos)?;
     println!("=> tok: {:?}", tok);
     match tok {
         Some(LexToken::Add(_)) => {
@@ -101,21 +128,22 @@ fn parse_expr(tokens: &Vec<LexToken>, pos: usize) -> Result<(ParseNode, usize), 
             expr_node.add_child_node(ParseNode::new(CfgTerm::TermMinus));
         }
         _ => {
-            return Err(format!("Expected plus sign!"));
+            return Err(ParseError::InvalidTokenError(String::from(
+                "Expected +/- sign!",
+            )));
         }
     }
 
     // look for expr
     println!("=> looking for + expr ...");
-    let (tail_expr_node, tail_expr_pos) =
-        parse_expr(&tokens, new_pos + 2).expect("tail expression failed!");
+    let (tail_expr_node, tail_expr_pos) = parse_expr(&tokens, next_pos + 1)?;
     expr_node.add_child_node(tail_expr_node);
 
     Ok((expr_node, tail_expr_pos))
 }
 
 /// parsing expression involving multiply (or '*')
-fn parse_multi_expr(tokens: &Vec<LexToken>, pos: usize) -> Result<(ParseNode, usize), String> {
+fn parse_multi_expr(tokens: &Vec<LexToken>, pos: usize) -> Result<(ParseNode, usize), ParseError> {
     println!("=> parsing multi_expr node at position {pos}");
     // call term
     let (term_node, new_pos) = parse_term(tokens, pos)?;
@@ -125,7 +153,7 @@ fn parse_multi_expr(tokens: &Vec<LexToken>, pos: usize) -> Result<(ParseNode, us
 }
 
 /// parsing term (either number or sub expr)
-fn parse_term(tokens: &Vec<LexToken>, pos: usize) -> Result<(ParseNode, usize), String> {
+fn parse_term(tokens: &Vec<LexToken>, pos: usize) -> Result<(ParseNode, usize), ParseError> {
     println!("=> parsing term node at position {pos} ...");
     let tok = tokens.get(pos);
     match tok {
@@ -154,21 +182,30 @@ fn parse_term(tokens: &Vec<LexToken>, pos: usize) -> Result<(ParseNode, usize), 
             return Ok((pt_node, pos));
         }
         _ => {
-            return Err(format!("Error: invalid term token!"));
+            return Err(ParseError::InvalidTokenError(format!(
+                "Error: invalid term token: {}",
+                tok.unwrap()
+            )));
         }
     }
 }
 
 /// Construct parse tree from the list of lex tokens
-pub(crate) fn parse(tokens: Vec<LexToken>) -> Result<(ParseNode, usize), String> {
-    // start off with Expr
-    return parse_expr(&tokens, 0);
+pub(crate) fn parse(tokens: Vec<LexToken>) -> Result<ParseNode, ParseError> {
+    // start off with Exprs (start rule)
+    match parse_expr(&tokens, 0) {
+        Ok((parsed_node, _)) => {
+            return Ok(parsed_node);
+        }
+        Err(ParseError::EndOfTokenError) => Err(ParseError::EndOfTokenError),
+        _ => Err(ParseError::UnknownError),
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::cfg;
-    use crate::cfg::CfgTerm;
+    use crate::cfg::{CfgTerm, ParseError};
     use crate::lex;
 
     #[test]
@@ -177,43 +214,52 @@ mod tests {
         // parse nodes, including the non-terminals and terminals.
         let s = "2 + 3";
         let lex_tokens = lex::lexer(s).expect("Failed to tokenize the string!");
-        let (parsed_node, _) = cfg::parse(lex_tokens).expect("parsing failed!");
-        println!("parsed node: {}", parsed_node);
-        assert_eq!(parsed_node.current_node, CfgTerm::NonTermExpr);
-        assert_eq!(parsed_node.child_nodes.len(), 3);
-        let left_child_node = parsed_node
-            .child_nodes
-            .get(0)
-            .expect("Expected a terminal number node");
-        assert_eq!(left_child_node.current_node, CfgTerm::NonTermMultiExpr);
-        let left_child_num_node = left_child_node
-            .child_nodes
-            .get(0)
-            .expect("Expected a terminal number node");
-        assert_eq!(left_child_num_node.current_node, CfgTerm::TermNumber(2));
+        match cfg::parse(lex_tokens) {
+            Ok(parsed_node) => {
+                println!("parsed node: {}", parsed_node);
+                assert_eq!(parsed_node.current_node, CfgTerm::NonTermExpr);
+                assert_eq!(parsed_node.child_nodes.len(), 3);
+                let left_child_node = parsed_node
+                    .child_nodes
+                    .get(0)
+                    .expect("Expected a terminal number node");
+                assert_eq!(left_child_node.current_node, CfgTerm::NonTermMultiExpr);
+                let left_child_num_node = left_child_node
+                    .child_nodes
+                    .get(0)
+                    .expect("Expected a terminal number node");
+                assert_eq!(left_child_num_node.current_node, CfgTerm::TermNumber(2));
 
-        // '+' bit
-        let plus_child_node = parsed_node
-            .child_nodes
-            .get(1)
-            .expect("Expected a terminal + node");
-        assert_eq!(plus_child_node.current_node, CfgTerm::TermPlus);
+                // '+' bit
+                let plus_child_node = parsed_node
+                    .child_nodes
+                    .get(1)
+                    .expect("Expected a terminal + node");
+                assert_eq!(plus_child_node.current_node, CfgTerm::TermPlus);
 
-        // right side of plus, TermExpr -> TermMultiExpr -> TermNumber
-        let right_child_node = parsed_node
-            .child_nodes
-            .get(2)
-            .expect("Expected a terminal number node");
-        assert_eq!(right_child_node.current_node, CfgTerm::NonTermExpr);
-        let right_child_me_node = right_child_node
-            .child_nodes
-            .get(0)
-            .expect("Expected a Non terminal multi_expr node");
-        assert_eq!(right_child_me_node.current_node, CfgTerm::NonTermMultiExpr);
-        let right_child_num_node = right_child_me_node
-            .child_nodes
-            .get(0)
-            .expect("Expected a terminal number node");
-        assert_eq!(right_child_num_node.current_node, CfgTerm::TermNumber(3));
+                // right side of plus, TermExpr -> TermMultiExpr -> TermNumber
+                let right_child_node = parsed_node
+                    .child_nodes
+                    .get(2)
+                    .expect("Expected a terminal number node");
+                assert_eq!(right_child_node.current_node, CfgTerm::NonTermExpr);
+                let right_child_me_node = right_child_node
+                    .child_nodes
+                    .get(0)
+                    .expect("Expected a Non terminal multi_expr node");
+                assert_eq!(right_child_me_node.current_node, CfgTerm::NonTermMultiExpr);
+                let right_child_num_node = right_child_me_node
+                    .child_nodes
+                    .get(0)
+                    .expect("Expected a terminal number node");
+                assert_eq!(right_child_num_node.current_node, CfgTerm::TermNumber(3));
+            }
+            Err(ParseError::EndOfTokenError) => {
+                // end of token list
+            }
+            _ => {
+                println!("Unknown error!");
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 mod cfg;
 mod lex;
+use std::io::{self, Write};
 
 // grammar rules
 // start_rule: expr
 // expr: multi_expr + expr | multi_expr '-' expr
-// multi_expr: term
+// multi_expr: term * multi_expr | term
 // term: NUMBER | ( expr )
 
-fn main() {
-    let s = "2 - 3 - 4";
+fn main() -> io::Result<()> {
+    let mut s = String::new();
+    print!("Enter math expression to parse:\n>>");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut s)?;
+    println!("=> parsing expression: {s}");
     println!("=> run lexer ...");
-    let lex_tokens = lex::lexer(s).expect("Failed to tokenize the string!");
+    let lex_tokens = lex::lexer(s.trim()).expect("Failed to tokenize the string!");
     println!("lex tokens: {:?}", lex_tokens);
-    let (parse_tree, _) = cfg::parse(lex_tokens).expect("Unable to parse the given expression!");
-    println!("parse tree: {}", parse_tree);
+    match cfg::parse(lex_tokens) {
+        Ok(parse_tree) => {
+            println!("parse tree: {}", parse_tree);
+        }
+        Err(cfg::ParseError::EndOfTokenError) => {}
+        _ => {
+            println!("Unknown error occurred!")
+        }
+    }
+
+    Ok(())
 }
